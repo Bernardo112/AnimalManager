1c1f53b22d7fdc7a1c50ea316e22e9bae226aa65
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-11-16 14:01:02 -0300

    tela login

--- AnimalManager/Login.Designer.cs
+++ AnimalManager/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AnimalManager
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -44,8 +44,8 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Stencil", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Yellow;
-            this.label1.Location = new System.Drawing.Point(353, 49);
+            this.label1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label1.Location = new System.Drawing.Point(516, 216);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(167, 57);
             this.label1.TabIndex = 0;
@@ -55,9 +55,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::AnimalManager.Properties.Resources.Telecommuting_amico;
-            this.pictureBox1.Location = new System.Drawing.Point(636, 49);
+            this.pictureBox1.Location = new System.Drawing.Point(940, 159);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox1.Size = new System.Drawing.Size(450, 450);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -65,7 +65,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(332, 185);
+            this.textBox1.Location = new System.Drawing.Point(495, 352);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(236, 25);
@@ -73,7 +73,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(332, 244);
+            this.textBox2.Location = new System.Drawing.Point(495, 411);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(236, 27);
@@ -81,10 +81,10 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.button1.BackColor = System.Drawing.Color.Goldenrod;
             this.button1.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.SeaShell;
-            this.button1.Location = new System.Drawing.Point(363, 301);
+            this.button1.Location = new System.Drawing.Point(526, 468);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(164, 34);
             this.button1.TabIndex = 4;
@@ -97,7 +97,7 @@
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(333, 159);
+            this.label2.Location = new System.Drawing.Point(496, 326);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(79, 22);
             this.label2.TabIndex = 5;
@@ -110,7 +110,7 @@
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label3.Location = new System.Drawing.Point(333, 219);
+            this.label3.Location = new System.Drawing.Point(496, 386);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(69, 22);
             this.label3.TabIndex = 6;
@@ -119,27 +119,27 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Gold;
-            this.panel1.Location = new System.Drawing.Point(1, 449);
+            this.panel1.Location = new System.Drawing.Point(1, 985);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1500, 35);
+            this.panel1.Size = new System.Drawing.Size(1950, 35);
             this.panel1.TabIndex = 7;
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.Transparent;
             this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.groupBox1.Location = new System.Drawing.Point(267, 31);
+            this.groupBox1.Location = new System.Drawing.Point(430, 198);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(346, 393);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             // 
-            // Form1
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightYellow;
-            this.ClientSize = new System.Drawing.Size(919, 487);
+            this.ClientSize = new System.Drawing.Size(1473, 640);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -149,7 +149,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
-            this.Name = "Form1";
+            this.Name = "Login";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
